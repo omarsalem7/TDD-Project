9904d09854503367f0fe1cbb2755be1e9478cf64
omarsalem7 <omarsalem0721@gmail.com>
2022-04-22 03:32:16 +0200

    add fizzbuz method

--- solver.rb
+++ solver.rb
@@ -9,5 +9,11 @@ class Solver
         word.reverse
     end
 
+    def fizzbuzz(n)
+        return "fizzbuzz" if (n % 3).zero? && (n % 5).zero?
+        return "fizz" if (n % 3).zero?
+        return "buzz" if (n % 5).zero?
+        return n.to_s
+    end
 end
 
