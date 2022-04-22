832d75a82f2c0b6867c43c94c1c96e526154c137
omarsalem7 <omarsalem0721@gmail.com>
2022-04-22 03:12:23 +0200

    Add TDD for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,9 +4,15 @@ describe "test solver" do
     before(:each) do
         @solve = Solver.new
     end
+
     it "tets factorial" do 
     result= @solve.factorial(5)
     expect(result).to eq(120)
+    end
+
+    it "test reverse method by hello word" do
+        result= @solve.reverse("hello")
+        expect(result).to eq("olleh")
     end
 
 end
