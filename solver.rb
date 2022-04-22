14aaf12282ec638c4e6ea59ba2633f1a581497f4
omarsalem7 <omarsalem0721@gmail.com>
2022-04-22 03:23:49 +0200

    add TDD for fizbuzz method

--- solver.rb
+++ solver.rb
@@ -8,5 +8,6 @@ class Solver
     def reverse(word)
         word.reverse
     end
+
 end
 
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,4 +15,26 @@ describe "test solver" do
         expect(result).to eq("olleh")
     end
 
+    context 'test the fizzbuzz method' do
+        it "test When N is divisible by 3 and 5, return fizzbuzz" do
+            result= @solve.fizzbuzz (15)
+            expect(result).to eq("fizzbuzz")
+        end
+
+        it "test When N is divisible by 3, return fizz" do
+            result= @solve.fizzbuzz (6)
+            expect(result).to eq("fizz")
+        end
+        
+        it "test When N is divisible by 5, return buzz" do
+            result= @solve.fizzbuzz (20)
+            expect(result).to eq("buzz")
+        end
+
+        it "test When Any other case, return N in string" do
+            result= @solve.fizzbuzz (2)
+            expect(result).to eq("2")
+        end
+    end
+
 end
