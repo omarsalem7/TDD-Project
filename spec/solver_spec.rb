3bbd97d2779b9b35eccd8f2bace3961d842aeab3
omarsalem7 <omarsalem0721@gmail.com>
2022-04-22 03:08:23 +0200

    fix test solver by making a global instance of the class solver

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,9 +1,12 @@
 require_relative '../solver.rb'
 
 describe "test solver" do
+    before(:each) do
+        @solve = Solver.new
+    end
     it "tets factorial" do 
-    solve=Solver.new
-    result=solve.factorial(5)
+    result= @solve.factorial(5)
     expect(result).to eq(120)
     end
+
 end
